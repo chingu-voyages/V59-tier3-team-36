bbe13d29ba5068636d372858ef3d2654c7c86442
Banto Klara <bantoklara2@gmail.com>
2026-01-27 22:18:28 +0200

    Create database connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/database.js';
 
 // Creating express object
 const app = express();
@@ -17,6 +18,12 @@ app.get('/', (req, res) => {
 // Port Number
 const PORT = process.env.PORT || 5000;
 
-// Server Setup
-app.listen(PORT, console.log(
-    `Server started on port ${PORT}`));
+const startServer = async () => {
+    await connectDB();
+
+    // Server Setup
+    app.listen(PORT, console.log(
+        `Server started on port ${PORT}`));
+};
+
+startServer();
