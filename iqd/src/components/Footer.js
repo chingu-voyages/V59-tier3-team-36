de17382e66977683130d401ee6eb237900e9e039
Blackmile <stanley.ezeifeanyi@gmail.com>
2026-01-28 01:02:12 +0100

    add components with footer and navbar

---
+++ iqd/src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 border-t">
+      <div className="max-w-7xl mx-auto px-4 py-6 text-center text-sm text-gray-600">
+         View on Github||Team Members. All rights reserved.
+      </div>
+    </footer>
+  );
+}
