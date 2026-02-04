a2085f854df2736d184a7a06c924d3d95bb64d87
Banto Klara <bantoklara2@gmail.com>
2026-02-04 11:59:47 +0200

    Remove hardcoded backend api URL and use env variable instead

--- frontend/src/api/roles.js
+++ frontend/src/api/roles.js
@@ -1,5 +1,6 @@
 export const fetchRoles = async () => {
-    const response = await fetch('/api/roles');
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const response = await fetch(`${BASE_URL}/api/roles`);
 
     if (!response.ok) {
         throw new Error('Failed to fetch roles');
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8080"
-    }
-  },
   test: {
     environment: 'jsdom',
     globals: true,
