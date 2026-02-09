2db6f8bd77aa69c826013148ed115f3a2c9ce427
Kevin Llanos <kllano2@wgu.edu>
2026-02-09 09:34:55 -0600

    Update Navbar alignment and spacing version 2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,13 +19,13 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white shadow-md">
-      <div className="max-w-12xl mx-auto px-4">
+      <div className="max-w-12xl mx-auto px-2">
         <div className="flex flex-col md:flex-row md:justify-between md:items-center h-auto md:h-16 py-2 md:py-0">
           <div className="flex flex-col px-2 items-center md:items-start text-center md:text-left">
-            <div className="text-lg sm:text-xl md:text-2xl font-bold text-black break-words max-w-xs sm:max-w-md md:max-w-full">
+            <div className="text-lg sm:text-xl md:text-2xl font-bold text-black break-words max-w-xs sm:max-w-md md:max-w-full mb-1">
               AI Interview Question Generator
             </div>
-            <div className="text-xs text-gray-600 mt-1 md:mt-0">
+            <div className="text-xs text-gray-600 mt-4 md:mt-0">
               {dateString}
             </div>
           </div>
