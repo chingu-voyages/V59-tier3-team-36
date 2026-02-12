c4829824150a2c31efa25cefd025e94760c59694
shrusred <reddy.shruthi35@gmail.com>
2026-02-12 12:50:43 -0500

    modified router with answer check post endpoint

--- backend/routes/sessionRoute.js
+++ backend/routes/sessionRoute.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getSummary } from "../controllers/sessionController.js";
+import { getSummary, submitAnswer } from "../controllers/sessionController.js";
 import { validateObjectId } from "../middleware/validateObjectId.js";
 
 const router = Router();
 
+//GET /api/sessions/:id/summary
 router.get("/:id/summary", validateObjectId("id"), getSummary);
+
+//POST /api/sessions/:sessionId/answers
+router.post("/:sessionId/answers", validateObjectId("sessionId"), submitAnswer);
 
 export default router;
