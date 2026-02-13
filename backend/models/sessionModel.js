3e37ada082c41d4b14c80b0b18d678127fabe7df
shrusred <reddy.shruthi35@gmail.com>
2026-02-12 23:42:15 -0500

    resolved merge conflicts in sessionModel and updated it

--- backend/models/sessionModel.js
+++ backend/models/sessionModel.js
@@ -14,27 +14,15 @@ const answerSchema = new mongoose.Schema(
   { _id: false }
 );
 
-<<<<<<< HEAD
-const sessionSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-  role: { type: String, required: true },
-  answers: { type: [answerSchema], default: [] }, //changed from required: true to default:[] to allow for cases where this is no answer in the beginning of session
-});
+const sessionSchema = new mongoose.Schema(
+  {
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    role: { type: String, required: true },
+    answers: { type: [answerSchema], default: [] }, //changed from required: true to default:[] to allow for cases where this is no answer in the beginning of session
+  },
+  { timestamps: true }
+);
 
 const Sessions =
   mongoose.models.Sessions || mongoose.model("Sessions", sessionSchema); //OR ensures the Sessions model exists exactly once
 export default Sessions;
-=======
-const sessionSchema = new mongoose.Schema(
-    {
-        _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-        role: { type: String, required: true },
-        answers: { type: [answerSchema], required: true },
-    },
-    { timestamps: true }
-)
-
-const Sessions =
-    mongoose.models.Sessions || mongoose.model("Sessions", sessionSchema); //OR ensures the Sessions model exists exactly once
-export default Sessions;
->>>>>>> dev
